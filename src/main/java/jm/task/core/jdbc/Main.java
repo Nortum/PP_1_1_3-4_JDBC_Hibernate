5dc51ee01aa1e7dde5c366f09d34e59f41a1fc8d
Nortum <varevtsev@yandex.ru>
2022-03-12 13:08:30 +0400

    113

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+
+        userService.saveUser("Bob", "Smith", (byte) 30);
+        userService.saveUser("Rob", "Moor", (byte) 31);
+        userService.saveUser("Ann", "Macgroth", (byte) 32);
+        userService.saveUser("Lisa", "Stillman", (byte) 33);
+
+        userService.removeUserById(2);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
