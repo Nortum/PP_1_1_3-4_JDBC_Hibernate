959001f2b8950fcc937cc423ff78d4e90c21b8c6
Nortum <varevtsev@yandex.ru>
2022-03-24 14:10:59 +0400

    114

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,89 +1,89 @@
-package jm.task.core.jdbc.dao;
-
-import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-
-
-public class UserDaoJDBCImpl implements UserDao {
-    private final Connection connection = Util.getConnection();
-
-    public UserDaoJDBCImpl() {
-
-    }
-
-    public void createUsersTable() {
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS mydb.users " +
-                    "(id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), lastName VARCHAR(255), age INT)");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void dropUsersTable() {
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate("drop table if exists users");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void saveUser(String name, String lastName, byte age) {
-        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO mydb.users (name, lastName, age) VALUES (?, ?, ?)")) {
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.setByte(3, age);
-            preparedStatement.executeUpdate();
-            System.out.println("User с именем – " + name + " добавлен в базу данных");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void removeUserById(long id) {
-        try (Statement statement = connection.createStatement()) {
-            String sql = "DELETE FROM users WHERE id";
-            statement.executeUpdate(sql);
-            System.out.println("User удален");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public List<User> getAllUsers() {
-        List<User> allUser = new ArrayList<>();
-        String sql = "SELECT id, name, lastName, age FROM users";
-
-        try (Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery(sql);
-
-            while (resultSet.next()) {
-                User user = new User();
-                user.setId(resultSet.getLong("id"));
-                user.setName(resultSet.getString("name"));
-                user.setLastName(resultSet.getString("lastName"));
-                user.setAge(resultSet.getByte("age"));
-                allUser.add(user);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return allUser;
-    }
-
-    public void cleanUsersTable() {
-        String sql = "TRUNCATE users";
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate(sql);
-            System.out.println("Таблица очищена");
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Не удалось очистить");
-        }
-    }
-}
+//package jm.task.core.jdbc.dao;
+//
+//import jm.task.core.jdbc.model.User;
+//import jm.task.core.jdbc.util.Util;
+//
+//import java.sql.*;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//
+//public class UserDaoJDBCImpl implements UserDao {
+//    private final Connection connection = Util.getConnection();
+//
+//    public UserDaoJDBCImpl() {
+//
+//    }
+//
+//    public void createUsersTable() {
+//        try (Statement statement = connection.createStatement()) {
+//            statement.executeUpdate("CREATE TABLE IF NOT EXISTS mydb.users " +
+//                    "(id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), lastName VARCHAR(255), age INT)");
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public void dropUsersTable() {
+//        try (Statement statement = connection.createStatement()) {
+//            statement.executeUpdate("drop table if exists users");
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public void saveUser(String name, String lastName, byte age) {
+//        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO mydb.users (name, lastName, age) VALUES (?, ?, ?)")) {
+//            preparedStatement.setString(1, name);
+//            preparedStatement.setString(2, lastName);
+//            preparedStatement.setByte(3, age);
+//            preparedStatement.executeUpdate();
+//            System.out.println("User с именем – " + name + " добавлен в базу данных");
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public void removeUserById(long id) {
+//        try (Statement statement = connection.createStatement()) {
+//            String sql = "DELETE FROM users WHERE id";
+//            statement.executeUpdate(sql);
+//            System.out.println("User удален");
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public List<User> getAllUsers() {
+//        List<User> allUser = new ArrayList<>();
+//        String sql = "SELECT id, name, lastName, age FROM users";
+//
+//        try (Statement statement = connection.createStatement()) {
+//            ResultSet resultSet = statement.executeQuery(sql);
+//
+//            while (resultSet.next()) {
+//                User user = new User();
+//                user.setId(resultSet.getLong("id"));
+//                user.setName(resultSet.getString("name"));
+//                user.setLastName(resultSet.getString("lastName"));
+//                user.setAge(resultSet.getByte("age"));
+//                allUser.add(user);
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return allUser;
+//    }
+//
+//    public void cleanUsersTable() {
+//        String sql = "TRUNCATE users";
+//        try (Statement statement = connection.createStatement()) {
+//            statement.executeUpdate(sql);
+//            System.out.println("Таблица очищена");
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//            System.out.println("Не удалось очистить");
+//        }
+//    }
+//}
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,11 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,16 +14,22 @@ public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/mydb";
     private static final String USER = "root";
     private static final String PASSWORD = "1234";
-    public static Connection connection;
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static SessionFactory sessionFactory;
 
-    public static Connection getConnection() {
-        Connection connection = null;
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return connection;
+    public static SessionFactory getConnection() {
+        Configuration configuration = new Configuration()
+                .setProperty("hibernate.connection.driver_class", DRIVER)
+                .setProperty("hibernate.connection.url", URL)
+                .setProperty("hibernate.connection.username", USER)
+                .setProperty("hibernate.connection.password", PASSWORD)
+                .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                .addAnnotatedClass(User.class);
+
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties()).build();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+        return sessionFactory;
     }
 }
